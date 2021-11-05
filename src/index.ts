81af6c8d94aa6183118f16a26a82192f8cb87015
Coyls <loic.grecofr@gmail.com>
2021-11-05 22:15:32 +0100

    refactor(post): start adding post request

--- src/index.ts
+++ src/index.ts
@@ -53,13 +53,40 @@ app.listen(port, () => {
       res.send(dataTest);
     });
 
-    console.log(dataTest);
+    // console.log(dataTest);
+
+    /////////////////////////////////////////////////////////////////////////
+    app.post("/testpost", async (req, res) => {
+      const result = await session.run(
+        "MATCH (year:Year) WHERE year.year = $year MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year) MATCH (population:Population)-[:POPULATION_IN_YEAR]->(year) MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal) WHERE medal.type <> $type MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete) MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country) MATCH (gpd)-[:GPD_IN_COUNTRY]->(country) MATCH (population)-[:POPULATION_IN_COUNTRY]->(country) RETURN DISTINCT country.name as Country, year.year as Year, count(medal) as Medals, population.value as Population, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC",
+        { year: "2016", type: "none" }
+      );
+
+      const allRecords = result.records;
+
+      const dataTest = allRecords.map((rec) => {
+        const medals = rec.get(2);
+
+        return {
+          year: rec.get(1),
+          country: rec.get(0),
+          medals: medals.low,
+          population: rec.get(3),
+          gpd: rec.get(4),
+        };
+      });
+
+      res.send("Send");
+
+      console.log(req);
+    });
+    /////////////////////////////////////////////////////////////////////////
   } finally {
-    await session.close();
+    // await session.close();
   }
 
   // on application exit:
-  await driver.close();
+  // await driver.close();
 })();
 
 // A DELETE QUAND JE SERAIS SUR DE MON CODE...
