2845134f978784407ae8476589b96b9ae9daa73f
Coyls <loic.grecofr@gmail.com>
2021-11-05 14:55:17 +0100

    feat(test-api): create test-api send data

--- cypher/scripts.cypher
+++ cypher/scripts.cypher
@@ -2,9 +2,11 @@
 
 MATCH (year:Year) WHERE year.year = "2016"
 MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year)
+MATCH (population:Population)-[:POPULATION_IN_YEAR]->(year)
 MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal)
 WHERE medal.type <> "none"
 MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete)
 MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country)
 MATCH (gpd)-[:GPD_IN_COUNTRY]->(country)
-RETURN DISTINCT country.name as Country, year.year as Year, count(medal) as Medals, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC
+MATCH (population)-[:POPULATION_IN_COUNTRY]->(country)
+RETURN DISTINCT country.name as Country, year.year as Year, count(medal) as Medals, population.value as Population, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import neo4j from "neo4j-driver";
+import cors from "cors";
 
 const app = express();
 const port = 3000 || process.env.PORT;
+
+app.use(cors());
+
+/* use((req, resp, next) => {
+  next();
+}, cors({ maxAge: 84600 })); */
 
 console.log(process.env.NODE_ENV);
 
@@ -21,30 +28,42 @@ app.listen(port, () => {
   );
   const session = driver.session();
 
+  // METTRE TOUS CE QUI CE TROUVE DANS LE TRY DANS UNE FONCTION
   try {
     const result = await session.run(
-      "MATCH (year:Year) WHERE year.year = $year MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year) MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal) WHERE medal.type <> $type MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete) MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country) MATCH (gpd)-[:GPD_IN_COUNTRY]->(country) RETURN DISTINCT country.name as Country, year.year as Year, count(medal) as Medals, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC ",
+      "MATCH (year:Year) WHERE year.year = $year MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year) MATCH (population:Population)-[:POPULATION_IN_YEAR]->(year) MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal) WHERE medal.type <> $type MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete) MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country) MATCH (gpd)-[:GPD_IN_COUNTRY]->(country) MATCH (population)-[:POPULATION_IN_COUNTRY]->(country) RETURN DISTINCT country.name as Country, year.year as Year, count(medal) as Medals, population.value as Population, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC",
       { year: "2016", type: "none" }
     );
 
-    const singleRecord = result.records[0];
     const allRecords = result.records;
-    const node = singleRecord.get(0);
 
-    app.get("/test", (req, res) => {
-      res.send(allRecords);
+    const dataTest = allRecords.map((rec) => {
+      const medals = rec.get(2);
+
+      return {
+        year: rec.get(1),
+        country: rec.get(0),
+        medals: medals.low,
+        population: rec.get(3),
+        gpd: rec.get(4),
+      };
     });
 
-    console.log(node.properties);
+    app.get("/test", (req, res) => {
+      res.send(dataTest);
+    });
+
+    console.log(dataTest);
   } finally {
     await session.close();
   }
 
   // on application exit:
   await driver.close();
-
-  /* 
-        "start": "node -r dotenv/config ./server/index.js",
-        "dev": "nodemon -r dotenv/config ./server/index.js",
-        */
 })();
+
+// A DELETE QUAND JE SERAIS SUR DE MON CODE...
+/* 
+      "start": "node -r dotenv/config ./server/index.js",
+      "dev": "nodemon -r dotenv/config ./server/index.js",
+      */
