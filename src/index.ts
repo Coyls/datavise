47ff8ea51ebe4c9db87c5d3c8595ea2647b261fb
Coyls <loic.grecofr@gmail.com>
2021-11-07 14:23:04 +0100

    feat(docker and post): change order lauch container and structure post method

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const app = express();
 const port = 3000 || process.env.PORT;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 /* use((req, resp, next) => {
   next();
@@ -26,37 +28,12 @@ app.listen(port, () => {
     "bolt://neo4j:7687",
     neo4j.auth.basic(process.env.USER_NEO4J, process.env.MDP_NEOJ4)
   );
-  const session = driver.session();
 
-  // METTRE TOUS CE QUI CE TROUVE DANS LE TRY DANS UNE FONCTION
-  try {
-    const result = await session.run(
-      "MATCH (year:Year) WHERE year.year = $year MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year) MATCH (population:Population)-[:POPULATION_IN_YEAR]->(year) MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal) WHERE medal.type <> $type MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete) MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country) MATCH (gpd)-[:GPD_IN_COUNTRY]->(country) MATCH (population)-[:POPULATION_IN_COUNTRY]->(country) RETURN DISTINCT country.name as Country, year.year as Year, count(medal) as Medals, population.value as Population, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC",
-      { year: "2016", type: "none" }
-    );
+  // GET
+  app.get("/test", async (req, res) => {
+    const session = driver.session();
 
-    const allRecords = result.records;
-
-    const dataTest = allRecords.map((rec) => {
-      const medals = rec.get(2);
-
-      return {
-        year: rec.get(1),
-        country: rec.get(0),
-        medals: medals.low,
-        population: rec.get(3),
-        gpd: rec.get(4),
-      };
-    });
-
-    app.get("/test", (req, res) => {
-      res.send(dataTest);
-    });
-
-    // console.log(dataTest);
-
-    /////////////////////////////////////////////////////////////////////////
-    app.post("/testpost", async (req, res) => {
+    try {
       const result = await session.run(
         "MATCH (year:Year) WHERE year.year = $year MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year) MATCH (population:Population)-[:POPULATION_IN_YEAR]->(year) MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal) WHERE medal.type <> $type MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete) MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country) MATCH (gpd)-[:GPD_IN_COUNTRY]->(country) MATCH (population)-[:POPULATION_IN_COUNTRY]->(country) RETURN DISTINCT country.name as Country, year.year as Year, count(medal) as Medals, population.value as Population, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC",
         { year: "2016", type: "none" }
@@ -75,18 +52,40 @@ app.listen(port, () => {
           gpd: rec.get(4),
         };
       });
+      res.send(dataTest);
+    } finally {
+      await session.close();
+    }
+  });
 
-      res.send("Send");
+  // POST
+  app.post("/testpost", async (req, res) => {
+    const session = driver.session();
+    try {
+      const result = await session.run(
+        "MATCH (year:Year) WHERE year.year = $year MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year) MATCH (population:Population)-[:POPULATION_IN_YEAR]->(year) MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal) WHERE medal.type <> $type MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete) MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country) MATCH (gpd)-[:GPD_IN_COUNTRY]->(country) MATCH (population)-[:POPULATION_IN_COUNTRY]->(country) RETURN DISTINCT country.name as Country, year.year as Year, count(medal) as Medals, population.value as Population, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC",
+        { year: req.body.year, type: "none" }
+      );
 
-      console.log(req);
-    });
-    /////////////////////////////////////////////////////////////////////////
-  } finally {
-    // /!\/!\/!\/!\/!\ A REMETTRE /!\/!\/!\/!\/!\
-    // await session.close();
-  }
+      const allRecords = result.records;
 
-  // /!\/!\/!\/!\/!\ A REMETTRE /!\/!\/!\/!\/!\
-  // on application exit:
-  // await driver.close();
+      const dataTest = allRecords.map((rec) => {
+        const medals = rec.get(2);
+
+        return {
+          year: rec.get(1),
+          country: rec.get(0),
+          medals: medals.low,
+          population: rec.get(3),
+          gpd: rec.get(4),
+        };
+      });
+
+      res.send(dataTest);
+    } finally {
+      await session.close();
+    }
+  });
+
+  await driver.close();
 })();
