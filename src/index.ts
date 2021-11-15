4d84082464cc4becb809106fa61b76357ab7bc46
Coyls <loic.grecofr@gmail.com>
2021-11-15 09:08:22 +0100

    refactor(get): refactor post method in get

--- src/index.ts
+++ src/index.ts
@@ -33,10 +33,12 @@ app.listen(port, () => {
   app.get("/test", async (req, res) => {
     const session = driver.session();
 
+    const year = req.query?.year ? req.query.year : "2016";
+
     try {
       const result = await session.run(
         "MATCH (year:Year) WHERE year.year = $year MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year) MATCH (population:Population)-[:POPULATION_IN_YEAR]->(year) MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal) WHERE medal.type <> $type MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete) MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country) MATCH (gpd)-[:GPD_IN_COUNTRY]->(country) MATCH (population)-[:POPULATION_IN_COUNTRY]->(country) RETURN DISTINCT country.iso as Country, year.year as Year, count(medal) as Medals, population.value as Population, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC",
-        { year: "2016", type: "none" }
+        { year, type: "none" }
       );
 
       const allRecords = result.records;
@@ -52,35 +54,6 @@ app.listen(port, () => {
           gpd: rec.get(4),
         };
       });
-      res.send(dataTest);
-    } finally {
-      await session.close();
-    }
-  });
-
-  // POST
-  app.post("/testpost", async (req, res) => {
-    const session = driver.session();
-    try {
-      const result = await session.run(
-        "MATCH (year:Year) WHERE year.year = $year MATCH (gpd:Gpd)-[:GPD_IN_YEAR]->(year) MATCH (population:Population)-[:POPULATION_IN_YEAR]->(year) MATCH (year)<-[:JO_IN_YEAR]-(jo:Jo)<-[:MEDAL_IN_JO]-(medal:Medal) WHERE medal.type <> $type MATCH (medal)-[:MEDAL_WIN_BY_ATHLETE]->(athlete:Athlete) MATCH (athlete)-[:ATHLETE_FROM_COUNTRY]->(country:Country) MATCH (gpd)-[:GPD_IN_COUNTRY]->(country) MATCH (population)-[:POPULATION_IN_COUNTRY]->(country) RETURN DISTINCT country.iso as Country, year.year as Year, count(medal) as Medals, population.value as Population, gpd.value as GPD ORDER BY toInteger(gpd.value) DESC",
-        { year: req.body.year, type: "none" }
-      );
-
-      const allRecords = result.records;
-
-      const dataTest = allRecords.map((rec) => {
-        const medals = rec.get(2);
-
-        return {
-          year: rec.get(1),
-          country: rec.get(0),
-          medals: medals.low,
-          population: rec.get(3),
-          gpd: rec.get(4),
-        };
-      });
-
       res.send(dataTest);
     } finally {
       await session.close();
